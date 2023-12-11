9a7340a27dbb51852dcde074f4b6a2da4a26f830
Peter Szerzo <szerzo.peter@gmail.com>
2023-12-11 14:59:49 +0100

    Improve layout. Hero Alpine interactions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        highlight: `0 0 0 4px ${colors.purple["200"]}`,
+      },
+    },
   },
   plugins: [],
 };
