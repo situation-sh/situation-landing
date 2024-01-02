059c268337456afb2d28587d0b4a21f81f010e72
Alban Siffer <alban.siffer@irisa.fr>
2024-01-02 15:44:15 +0100

    degraded mode begins

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
       boxShadow: {
         highlight: `0 0 0 4px ${colors.purple["200"]}`,
       },
+      colors: {
+        // primary: "#c3bbec",
+        primary: "#c4a7d9",
+        // primary: "#e7cee9",
+      },
     },
   },
   plugins: [],
